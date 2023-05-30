72a04eaff336a166a4429c2786f71670b0821dfb
JC <21CO31239401@campus.economicas.uba.ar>
2023-05-30 15:39:39 -0300

    se agrega la clase persona

---
+++ EjerciocioPrueba/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciocioPrueba
+{
+    internal class Persona
+    {
+        public class Humano
+        {
+            // Campos de la clase
+            public string Nombre;
+            public int Edad;
+
+            // Constructor de la clase
+            public Humano(string nombre, int edad)
+            {
+                Nombre = nombre;
+                Edad = edad;
+            }
+
+            // Métodos de la clase
+            public void Saludo()
+            {
+                Console.WriteLine("Hola, mi nombre es " + Nombre + " y tengo " + Edad + " años.");
+            }
+        }
+    }
+}
--- EjerciocioPrueba/Program.cs
+++ EjerciocioPrueba/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EjerciocioPrueba;
+
 Console.WriteLine("Hola Mundo!");
 Console.WriteLine("Por favor ingrese su nombre");
 Console.ReadLine();
 Console.WriteLine("Presione Enter para salir");
 Console.ReadKey();
 
+Persona Alumno = new Persona ();
 
 
 
